f27f5d24475cc4e5e71cd4628288bdd26346df0c
daffa akbar <daffaakbar5@gmail.com>
2021-02-17 22:37:04 +0700

    method

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
-app.use( ()=>{
-    console.log('server')
+//Semua method
+router.use('/products', (req,res, next)=> {
+    // console.log('url: ', req.originalUrl);
+    // console.log('method: ', req.method);
+    res.json({name: "Daffa Akbar", email: "da@gmail.com"})
+    next();
 })
+router.use('/price', (req,res, next)=> {
+    res.json({price: 300000})
+    next();
+})
+//Hanya method GET
+router.get('/customers', (req,res, next) => {
+    res.json({title: "Customer"})
+    next();
+})
+
+app.use('/', router);
 
 app.listen(4000);
