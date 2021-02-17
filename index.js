3bbeedaee26bfcf68c409b4d5f09db9f4dd301d2
daffa akbar <daffaakbar5@gmail.com>
2021-02-17 22:07:30 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use( ()=>{
+    console.log('Hello serverrr')
+    console.log('Hello serverrr')
+    console.log('Hello serverrr')
+    console.log('Hello serasdaverrr')
+})
+
+app.listen(4000);
