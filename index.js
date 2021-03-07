c0850ce021b47a8b635f036f2e3d7c69c0b1e755
daffa akbar <daffaakbar5@gmail.com>
2021-03-07 15:30:37 +0700

    first commit

--- index.js
+++ index.js
@@ -1,25 +1,77 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+//Connect MongoDB
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://daffa:5solikin@cluster0.9wqvd.mongodb.net/blog?retryWrites=true&w=majority";
+// handle form-data
+const multer = require('multer'); 
+// handle req image
+const path = require('path');
 
 const app = express();
-const router = express.Router();
+// const productRoutes = require('./src/routes/products')
+const authRoutes = require('./src/routes/auth')
+const blogRoutes = require('./src/routes/blog');
 
-//Semua method
-router.use('/products', (req,res, next)=> {
-    // console.log('url: ', req.originalUrl);
-    // console.log('method: ', req.method);
-    res.json({name: "Daffa Akbar", email: "da@gmail.com"})
+// handle form-image
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().getTime() + '-' + file.originalname)
+    }
+})
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ){
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+// config
+app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 })
-router.use('/price', (req,res, next)=> {
-    res.json({price: 300000})
-    next();
-})
-//Hanya method GET
-router.get('/customers', (req,res, next) => {
-    res.json({title: "Customer"})
-    next();
-})
 
-app.use('/', router);
+// app.use('/v1', productRoutes);
+app.use('/v1/auth', authRoutes);
+app.use('/v1/blog', blogRoutes);
 
-app.listen(4000);
+//handle error
+app.use((error, req, res, next) =>{
+    const status = error.errorStatus || 500;
+    const message = error.message;
+    const data = error.data;
+
+    res.status(status).json({
+        message: message,
+        data: data
+    })
+});
+
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   app.listen(4000, () => console.log('Koneksi sukses'));
+
+//   client.close();
+// });
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=> {
+    app.listen(4000, () => console.log('Koneksi sukses'));
+
+})
+.catch(err => console.log(err));
+
---
+++ src/routes/blog.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const {body} = require('express-validator');
+
+const router = express.Router();
+
+const blogController = require('../controllers/blog');
+
+//[POST] : /v1/blog/post
+router.post('/post', [
+    body('title').isLength({min: 5}).withMessage('Input title Min. 5 karakter'),
+    body('body').isLength({min: 5}).withMessage('Input Body Min. 5 karakter ')
+],blogController.createBlogPost);
+
+//[GET] : /v1/blog/posts
+router.get('/posts', blogController.getAllBlogPost);
+router.get('/post/:postId', blogController.getBlogPostById);
+router.put('/post/:postId',[
+    body('title').isLength({min: 5}).withMessage('Input title Min. 5 karakter'),
+    body('body').isLength({min: 5}).withMessage('Input Body Min. 5 karakter ')
+], blogController.updateBlogPost);
+router.delete('/post/:postId', blogController.deleteBlogPost);
+
+module.exports = router;
